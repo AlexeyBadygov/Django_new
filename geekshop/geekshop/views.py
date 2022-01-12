a88220a5ac12cde494971e9f3d1e39f48122ccdb
AlexeyBadygov <kick@mail.ru>
2022-01-12 13:25:28 +0700

    lesson_6

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 import json
 
+from basketapp.models import Basket
 from mainapp.models import Product
 
 
 def index(request):
     title = 'Главная'
     products = Product.objects.all()[:4]
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
     context = {
         'title': title,
         'products': products,
+        'basket': basket,
     }
     return render(request, 'geekshop/index.html', context=context)
 
